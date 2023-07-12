c4fb95d11962a73a56b0c1550f8fc9e378d8fa79
nsbsb99 <solbin9910@gmail.com>
2023-07-12 15:23:59 +0900

    First

---
+++ Circus_Charlie/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D playerRigid = default;
+    private float jumpForce = 350f;
+    private int jumpCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody2D>();
+        Debug.Assert(playerRigid != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && jumpCount < 1)
+        {
+            jumpCount += 1;
+            playerRigid.velocity = Vector2.zero;
+            playerRigid.AddForce(new Vector2(0, jumpForce));
+        }
+
+        else if (Input.GetMouseButtonDown(0) && playerRigid.velocity.y > 0)
+        {
+            playerRigid.velocity = playerRigid.velocity * 0.5f;
+        }
+    }
+
+}
